7ca6e7b3d33fd07be444de453916212b5f42eb80
Arun Menon <arun.menon@qut.edu.au>
2020-05-18 06:10:39 +1000

    Adding maven profiles and refactor code

--- src/main/groovy/org/arunm/springwebfluxrestservice/client/ReactiveClientApplication.groovy
+++ src/main/groovy/org/arunm/springwebfluxrestservice/client/ReactiveClientApplication.groovy
@@ -1,7 +1,6 @@
 package org.arunm.springwebfluxrestservice.client
 
 import groovy.json.JsonOutput
-import groovy.json.JsonSlurper
 import org.arunm.springwebfluxrestservice.Event
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -9,7 +8,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.WebClient
-import reactor.core.publisher.Mono
 
 @SpringBootApplication
 class ReactiveClientApplication {
@@ -19,24 +17,26 @@ class ReactiveClientApplication {
         return WebClient.create("http://localhost:8080")
     }
 
-    CommandLineRunner getFluxDemo (WebClient client) {
+    @Bean
+    CommandLineRunner getFluxDemoRetrieve (WebClient client) {
         return { args ->
             client.get().uri("/events")
             .accept(MediaType.TEXT_EVENT_STREAM)
             .retrieve()
             .bodyToFlux(Event)
-            .subscribe { println it }
+            .subscribe { println "getFluxDemo $it" }
         }
     }
 
 
+    @Bean
     CommandLineRunner getFluxDemoExchange (WebClient client) {
         return { args ->
             client.get().uri("/events")
                     .accept(MediaType.TEXT_EVENT_STREAM)
                     .exchange()
                     .flatMapMany { response -> response.bodyToFlux(Event.class) }
-                    .subscribe { println it }
+                    .subscribe { println "getFluxDemoExchange --> $it" }
         }
     }
 
@@ -49,7 +49,7 @@ class ReactiveClientApplication {
                     .bodyValue(JsonOutput.toJson(new Event(3L)))
                     .retrieve()
                     .toBodilessEntity()
-                    .subscribe { println it}
+                    .subscribe { println "postDemo $it" }
         }
     }
 
